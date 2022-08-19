e62f3d6fc5b61853252c7ed9e8bad0f28c86a78d
eelcohn <eelco@huininga.nl>
2022-08-19 21:24:17 +0200

    Add files via upload

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -1,6 +1,6 @@
 /* eslint no-tabs: ["error", { allowIndentationTabs: true }] */
 
-import { config as config } from './config.js';
+import { loadConfig } from './config.js';
 import { createSystemMessage, removeSystemMessage } from './functions.js';
 import { Module as KNMI } from './modules/knmi.js';
 import { Module as KNMIGafor } from './modules/knmi_gafor.js';
@@ -13,7 +13,7 @@ import { Module as WeerLive } from './modules/weerlive.js';
 import { Module as WeerSlag } from './modules/weerslag.js';
 import { Module as WindsAloft } from './modules/windsaloft.js';
 
-const APPNAME = 'Skydive Weather Dashboard v0.0.1';
+const APPNAME = 'Skydive Weather Dashboard v0.0.2';
 const ID_DATETIME = 'datetime-data';
 const ID_IMG_LAYER_MAP = 'img-layer-map-id';
 const ID_IMG_LAYER_CLOUD = 'img-layer-cloud-id';
@@ -22,6 +22,7 @@ const ID_IFRAME_RAIN = 'layer-rain';
 const ID_UPPERWINDS_TABLE = 'uppper-winds-content-data';
 const ID_WEATHER_ALERT = 'weather-alert';
 const ID_COMPASS = 'compass';
+const ID_METAR = 'metar';
 const UNIT_ALTITUDE = 'ft';
 
 class ShowCurrentDateTime {
@@ -43,7 +44,7 @@ class ShowCurrentDateTime {
 		var element = document.getElementById(ID_DATETIME);
 		var x = new Date();
 
-		element.innerHTML = x.toLocaleString(config.locale, config.options);
+		element.innerHTML = x.toLocaleString(document.config.locale, document.config.options);
 	}
 }
 
@@ -89,44 +90,56 @@ console.log = function () {
 };
 
 /* Make config available for all modules */
-document.config = config;
+document.config = {};
+loadConfig().then(response => {
+	/* Enable/disable compass rose */
+	if (document.config.compass === true) {
+		document.getElementById(ID_COMPASS).style.display = 'block';
+	} else {
+		document.getElementById(ID_COMPASS).style.display = 'none';
+	}
 
-/* Enable/disable compass rose */
-if (document.config.compass === true) {
-	document.getElementById(ID_COMPASS).style.display = 'block';
-} else {
-	document.getElementById(ID_COMPASS).style.display = 'none';
-}
+	/* Enable/disable METAR & TAF */
+	if (document.config.metar) {
+		if (document.config.metar.length == 0) {
+			document.getElementById(ID_METAR).style.display = 'none';
+		} else {
+			document.getElementById(ID_METAR).style.display = 'block';
+			var metar = {};
+			for (var i = 0; i < document.config.metar.length; i++) {
+				metar[document.config.metar[i]] = new NOAAMETAR(document.config.metar[i]);
+			}
+		}
+	}
 
-/* Set up realtime clock */
-var mod_datetime = new ShowCurrentDateTime(ID_DATETIME);
+	/* Set up realtime clock */
+	var mod_datetime = new ShowCurrentDateTime(ID_DATETIME);
 
-/* Set up online/offline status monitor */
-var online_status = new OnlineStatus();
+	/* Set up online/offline status monitor */
+	var online_status = new OnlineStatus();
 
-/* Set up ADS-B module(s) */
-//var airplanes = new OpenSkyNetwork(document.config.airplanes);
+	/* Set up ADS-B module(s) */
+	//var airplanes = new OpenSkyNetwork(document.config.airplanes);
 
-/* Add altitudes to upper winds table */
-for (var i = 0; i < document.config.upperwinds.length; i++) {
-	document.getElementById(ID_UPPERWINDS_TABLE).innerHTML += `<tr>
+	/* Add altitudes to upper winds table */
+	if (document.config.upperwinds) {
+		for (var i = 0; i < document.config.upperwinds.length; i++) {
+			document.getElementById(ID_UPPERWINDS_TABLE).innerHTML += `<tr>
 <td><span class="windtext" id="wind` + document.config.upperwinds[i] + `-label">` + document.config.upperwinds[i] + `&nbsp;` + UNIT_ALTITUDE + `</span></td>
 <td><span class="winddirection" id="wind` + document.config.upperwinds[i] + `-direction"></span></td>
 <td><span class="windspeed" id="wind` + document.config.upperwinds[i] + `-speed"></span></td>
 <td><span class="windtemperature" id="wind` + document.config.upperwinds[i] + `-temperature"></span></td>
 </tr>`;
-}
+		}
+	}
 
-//var knmi = new KNMI(ID_IMG_LAYER_RAIN);
-var knmi_gafor = new KNMIGafor();
-//var sat24 = new Sat24(ID_IMG_LAYER_CLOUD);
-//var weatherandradar = new WeatherAndRadar(config, ID_IFRAME_RAIN);
-var metar = {};
-for (var i = 0; i < document.config.metar.length; i++) {
-	metar[document.config.metar[i]] = new NOAAMETAR(document.config.metar[i]);
-}
-//var openweathermap = new OpenWeatherMap();
-var weerlive = new WeerLive();
-var weerslag = new WeerSlag(ID_IMG_LAYER_MAP);
-var windsaloft = new WindsAloft();
+	//var knmi = new KNMI(ID_IMG_LAYER_RAIN);
+	var knmi_gafor = new KNMIGafor();
+	//var sat24 = new Sat24(ID_IMG_LAYER_CLOUD);
+	//var weatherandradar = new WeatherAndRadar(config, ID_IFRAME_RAIN);
+	//var openweathermap = new OpenWeatherMap();
+	var weerlive = new WeerLive();
+	var weerslag = new WeerSlag(ID_IMG_LAYER_MAP);
+	var windsaloft = new WindsAloft();
+})
 
