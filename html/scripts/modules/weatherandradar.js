93f0bd739fee73c84de77f77fd611112bb4cf0c7
eelcohn <eelco@huininga.nl>
2022-08-20 10:40:12 +0200

    Add files via upload

--- html/scripts/modules/weatherandradar.js
+++ html/scripts/modules/weatherandradar.js
@@ -1,9 +1,9 @@
 /* eslint no-tabs: ["error", { allowIndentationTabs: true }] */
 
 class Module {
-	constructor(config, id) {
+	constructor(id) {
 		this.iframe_id = iframe_id;
-		this.iframe_url = 'https://www.weatherandradar.co.uk/weather-map?center=' + config.location.lattitude + ',' + config.location.longitude + '&zoom=' + config.weatherandradar.zoom + '&layer=' + config.weatherandradar.layer;
+		this.iframe_url = 'https://www.weatherandradar.co.uk/weather-map?center=' + document.config.location.lattitude + ',' + document.config.location.longitude + '&zoom=' + document.config.weatherandradar.zoom + '&layer=' + document.config.weatherandradar.layer;
 		this.refreshInterval = 5 * 60 * 1000; // Refresh interval is 5 minutes
 
 		/* Schedule update of document content */
