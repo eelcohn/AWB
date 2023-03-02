0b57c5de309cf5eb266ae799f81d3a0dbf75cc50
eelcohn <eelco@huininga.nl>
2023-03-02 13:15:26 +0100

    Hide mouse cursor after 5 seconds of inactivity

--- rpi/install.sh
+++ rpi/install.sh
@@ -70,7 +70,7 @@ grep -qxF "@xset s off" /etc/xdg/lxsession/LXDE-pi/autostart || echo "@xset s of
 # Disable display power management system
 grep -qxF "@xset -dpms" /etc/xdg/lxsession/LXDE-pi/autostart || echo "@xset -dpms" >> /etc/xdg/lxsession/LXDE-pi/autostart
 # Hide mouse-cursor when idle
-grep -qxF "@unclutter -idle 1" /etc/xdg/lxsession/LXDE-pi/autostart || echo "@unclutter -idle 1" >> /etc/xdg/lxsession/LXDE-pi/autostart
+grep -qxF "@unclutter -idle 5" /etc/xdg/lxsession/LXDE-pi/autostart || echo "@unclutter -idle 1" >> /etc/xdg/lxsession/LXDE-pi/autostart
 # Autostart browser and load webpage
 grep -qxF "@chromium-browser --noerrdialogs --check-for-update-interval=31536000 --disable-features=TranslateUI --disable-restore-session-state --disable-session-crashed-bubble --disable-infobars --incognito --kiosk ${WEATHER_URL} &" /etc/xdg/lxsession/LXDE-pi/autostart || echo "@chromium-browser --noerrdialogs --disable-infobars --check-for-update-interval=31536000 --incognito --kiosk ${WEATHER_URL} &" >> /etc/xdg/lxsession/LXDE-pi/autostart
 #grep -qxF "@firefox-esr -foreground --kiosk ${WEATHER_URL} &" /etc/xdg/lxsession/LXDE-pi/autostart || echo "@firefox-esr -foreground --kiosk ${WEATHER_URL} &" >> /etc/xdg/lxsession/LXDE-pi/autostart
