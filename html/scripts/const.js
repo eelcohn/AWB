9233379a166ad0700c341cb1cfc7b753089a5987
eelcohn <eelco@huininga.nl>
2023-03-04 10:13:12 +0100

    Update const.js

--- html/scripts/const.js
+++ html/scripts/const.js
@@ -2,4 +2,7 @@
 
 const DATE_OPTIONS_UTC = { timeZone: 'UTC', timeZoneName: 'short', hour12: false, hour: '2-digit', minute: '2-digit'};
 const DATE_OPTIONS_LOCAL = { timeZoneName: 'short', hour12: false, hour: '2-digit', minute: '2-digit'};
-
+const UNIT_WIND_DIRECTION = '°';
+const UNIT_SPEED_KNOTS = 'kt';
+const UNIT_TEMPERATURE_CELCIUS = '°C';
+const UNIT_ALTITUDE_FEET = 'ft';
