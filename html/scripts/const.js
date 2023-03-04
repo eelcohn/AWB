72990348f77602cac46534370fc9b58bc2936103
eelcohn <eelco@huininga.nl>
2023-03-04 10:31:54 +0100

    Update const.js

--- html/scripts/const.js
+++ html/scripts/const.js
@@ -1,5 +1,6 @@
 /* eslint no-tabs: ["error", { allowIndentationTabs: true }] */
 
+const APPNAME = 'Skydive Weather Dashboard v0.0.2';
 const DATE_OPTIONS_UTC = { timeZone: 'UTC', timeZoneName: 'short', hour12: false, hour: '2-digit', minute: '2-digit'};
 const DATE_OPTIONS_LOCAL = { timeZoneName: 'short', hour12: false, hour: '2-digit', minute: '2-digit'};
 const UNIT_CELCIUS = '°C';
