a523de16f23ffae803e53a2fca0551762272b9b0
eelcohn <eelco@huininga.nl>
2023-07-09 09:21:19 +0200

    Update localip.php

--- html/localip.php
+++ html/localip.php
@@ -1,6 +1,7 @@
 <?php
 if( ($_SERVER['REMOTE_ADDR'] == '127.0.0.1') || (substr($_SERVER['REMOTE_ADDR'], 0, 3) == '10.') || (substr($_SERVER['REMOTE_ADDR'], 0, 7) == '172.16.') || (substr($_SERVER['REMOTE_ADDR'], 0, 8) == '192.168.')) {
-	echo($_SERVER['REMOTE_ADDR']);
+//	echo($_SERVER['REMOTE_ADDR']);
+	echo(getHostByName(getHostName()));
 } else {
 	die('Access denied from IP ' . $_SERVER['REMOTE_ADDR'] );
 }
