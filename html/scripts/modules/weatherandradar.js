cee790af20823dcb1ef3ca45e05f51c9a9058438
eelcohn <eelco@huininga.nl>
2024-01-28 09:58:35 +0100

    Update weatherandradar.js

--- html/scripts/modules/weatherandradar.js
+++ html/scripts/modules/weatherandradar.js
@@ -1,16 +1,18 @@
 /* eslint no-tabs: ["error", { allowIndentationTabs: true }] */
 
 class Module {
-	constructor(id) {
+	constructor(iframe_id) {
 		this.iframe_id = iframe_id;
-		this.iframe_url = 'https://www.weatherandradar.co.uk/weather-map?center=' + document.config.location.lattitude + ',' + document.config.location.longitude + '&zoom=' + document.config.weatherandradar.zoom + '&layer=' + document.config.weatherandradar.layer;
+		this.iframe_url = 'https://radar.wo-cloud.com/pwa/?zoom=' + document.config.weatherandradar.zoom + '&center=' + document.config.location.lattitude + ',' + document.config.location.longitude + '&tz=' + document.config.weatherandradar.timezone + '&tf=' + document.config.weatherandradar.timeformat + '&tempunit=' + document.config.weatherandradar.tempunit + '&windunit=' + document.config.weatherandradar.windunit + '&lang=' + document.config.locale.substr(0,2) + '&desktop=true&loop=true&hideControls=' + document.config.weatherandradar.hideControls + '&placemark=' + document.config.location.lattitude + ',' + document.config.location.longitude + '&period=' + document.config.weatherandradar.period;
 		this.refreshInterval = 5 * 60 * 1000; // Refresh interval is 5 minutes
 
-		/* Schedule update of document content */
+		/* Schedule update of document content -- not needed since weatherandradar updates itself */
+/*
 		this.task = setInterval(
 			this.updateData.bind(this),
 			this.refreshInterval
 		);
+*/
 
 		/* Initial fill of document content */
 		this.updateData();
@@ -23,4 +25,3 @@ class Module {
 };
 
 export { Module };
-
