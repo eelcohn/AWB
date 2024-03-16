6ef78997f999804f9d2be118c8215dab2f325ce6
eelcohn <eelco@huininga.nl>
2024-03-16 15:44:28 +0100

    Update fixoverscan.sh

--- rpi/fixoverscan.sh
+++ rpi/fixoverscan.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Alter overscan settings so the full-screen desktop is available on normal TV's
-sed -i 's/#overscan_left=16/overscan_left=40/' '/boot/config.txt'
-sed -i 's/#overscan_right=16/overscan_right=40/' '/boot/config.txt'
-sed -i 's/#overscan_top=16/overscan_top=24/' '/boot/config.txt'
-sed -i 's/#overscan_bottom=16/overscan_bottom=24/' '/boot/config.txt'
+sed -i 's/#overscan_left=16/overscan_left=40/' '/boot/firmware/config.txt'
+sed -i 's/#overscan_right=16/overscan_right=40/' '/boot/firmware/config.txt'
+sed -i 's/#overscan_top=16/overscan_top=24/' '/boot/firmware/config.txt'
+sed -i 's/#overscan_bottom=16/overscan_bottom=24/' '/boot/firmware/config.txt'
