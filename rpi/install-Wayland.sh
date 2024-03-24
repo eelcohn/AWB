038f8f37ea5bae1e27e4cd8d4ffeb720d1d38290
eelcohn <eelco@huininga.nl>
2024-03-24 16:29:38 +0100

    Update and rename install-wayland.sh to install-Wayland.sh

--- rpi/install-Wayland.sh
+++ rpi/install-Wayland.sh
@@ -12,15 +12,6 @@ then
 	exit
 fi
 
-# ----------------------------------
-# Check the current window manager
-# ----------------------------------
-if [[ "${XDG_CURRENT_DESKTOP}" != "WAYLAND" ]]
-then
-	echo "This script only works for the Wayland window manager, and you're using the ${XDG_SESSION_TYPE} window manager."
-	exit
-fi
-
 echo "$(date +%c) Configuring ${XDG_CURRENT_DESKTOP}" >> "${LOG_FILE}" 2>&1
 
 # ----------------------------------
