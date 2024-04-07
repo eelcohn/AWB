3e558115e35b107ff3cad1a5585e0b53460db4f7
eelcohn <eelco@huininga.nl>
2024-04-07 08:55:48 +0200

    Update install-Wayland.sh

--- rpi/install-Wayland.sh
+++ rpi/install-Wayland.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
 LOG_FILE="/var/log/${APP_NAME}/install.log"
-WAYFIRE_INI_FILE="~/.config/wayfire.ini"
+WAYFIRE_INI_FILE="/home/${SUDO_USER}/.config/wayfire.ini"
 
 # ----------------------------------
 # Check if user has root permissions
 # ----------------------------------
-if [ "$EUID" -ne 0 ]
+if [[ "$EUID" -ne 0 ]]
 then
-	echo "Please run as root"
+	echo "Please run as root" >> "${LOG_FILE}" 2>&1
+	exit
+fi
+
+# ----------------------------------
+# Check if wayfire.ini file exists
+# ----------------------------------
+if [[ ! -e "${WAYFIRE_INI_FILE}" ]]
+then
+	echo "Could not find wayfire.ini file." >> "${LOG_FILE}" 2>&1
+ 	echo "${WAYFIRE_INI_FILE}" >> "${LOG_FILE}" 2>&1
 	exit
 fi
 
@@ -17,6 +27,7 @@ echo "$(date +%c) Configuring Wayland" >> "${LOG_FILE}" 2>&1
 # ----------------------------------
 # Set screen-resolution to 1920x1080 and refresh rate to 50Hz
 # ----------------------------------
+echo "$(date +%c) Wayfire: setting display resolution" >> "${LOG_FILE}" 2>&1
 apt-get -y install crudini >> "${LOG_FILE}" 2>&1
 crudini --set "${WAYFIRE_INI_FILE}" "output:HDMI-A-1" "mode" "1920x1080@50000" >> "${LOG_FILE}" 2>&1
 crudini --set "${WAYFIRE_INI_FILE}" "output:HDMI-A-1" "position" "0,0" >> "${LOG_FILE}" 2>&1
@@ -25,6 +36,7 @@ crudini --set "${WAYFIRE_INI_FILE}" "output:HDMI-A-1" "transform" "normal" >> "$
 # ----------------------------------
 # Compile extra Wayfire plugins (needed for the hide-cursor plugin)
 # ----------------------------------
+echo "$(date +%c) Wayfire: compiling and installing Wayfire extra plugins" >> "${LOG_FILE}" 2>&1
 EXTRAS_PATH="/tmp/wayfire-plugins-extra"
 BUILD_PATH="${EXTRAS_PATH}/build"
 apt-get -y install cmake libvulkan-dev meson >> "${LOG_FILE}" 2>&1
@@ -59,5 +71,6 @@ crudini --set "${WAYFIRE_INI_FILE}" "core" "plugins" "autostart hide-cursor" >> 
 # -------------------------------------------------------
 # Autostart kiosk script
 # -------------------------------------------------------
+echo "$(date +%c) Wayfire: configuring autostart kiosk script" >> "${LOG_FILE}" 2>&1
 crudini --set "${WAYFIRE_INI_FILE}" "autostart" "delprofile" "rm -rf ~/.config/chromium" >> "${LOG_FILE}" 2>&1
 crudini --set "${WAYFIRE_INI_FILE}" "autostart" "kiosk" "chromium-browser --noerrdialogs --disable-infobars --check-for-update-interval=31536000 --enable-logging --kiosk http://127.0.0.1/" >> "${LOG_FILE}" 2>&1
